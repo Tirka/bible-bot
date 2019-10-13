f4f0a1dea61bc934511ba5aad15f86101ef8ffdd
Maksim Vykhota <m.vykhota@protonmail.com>
2019-10-13 23:50:18 +0300

    more proj settings + some logic

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import telegraf from 'telegraf'
 import { Pool } from 'pg'
 import { config as config_environment } from 'dotenv'
+import {  } from 'lodash'
 
 config_environment()
 
-const bot_token = process.env.TG_BOT_TOKEN
+const bot_token = process.env.TG_BOT_TOKEN!
 
 const bot = new telegraf(bot_token)
 
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    port: +process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
+    host: process.env.PGHOST,
+    port: +process.env.PGPORT!,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
 })
 
 bot.hears('test', async (ctx) => {
-    let z = await pool.query('SELECT * from key_english LIMIT 5')
-    ctx.reply(JSON.stringify(z.rows, null, 2))
+    const query = 'SELECT c, v, t from t_sin WHERE id > 1001001 AND id <= 1002000'
+    const genesis_1 = await pool.query(query)
+
+    const text =
+        genesis_1.rows
+        .map((row) => `${row.c}:${row.v}. ${row.t}`)
+        .reduce((acc, next) => acc + next + '\n', '')
+
+    ctx.reply(text)
 })
 
 bot.start((ctx) => ctx.reply('Start Command'))
